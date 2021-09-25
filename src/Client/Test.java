b406d686a70181b1152ab69769f73bfb0740bc30
Théo <theomarion08@gmail.com>
2021-09-25 14:06:14 +0200

    test add

--- src/Client/Test.java
+++ src/Client/Test.java
@@ -15,7 +15,6 @@ public class Test {
 	    client.startConnection("127.0.0.1", 6666);
 	    String response = client.sendMessage("hello server");
 		System.out.println(response);
-		assertEquals("hello client", response);
 	}
 
 }
